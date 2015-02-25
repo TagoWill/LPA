06cc2dfa8e3196ae5c53f4e7856fc01dac48e0ce
Tiago Andrade <tagowill@gmail.com>
2015-02-25 22:14:38 +0000

    v2 week3

--- weektree/weektree.c
+++ weektree/weektree.c
@@ -4,12 +4,15 @@
 #define N 51
 
 int pontosmaximo=0;
-int array[N][2];
+static int array[N][2];
+int continua=0;
 
-void calcularpontosmaximos(int valor ,int n){
-    
+void calcularpontosmaximos(int valor, int nivel ,int n){
+    if(continua==0){
+    //printf("nivel - %d\n",nivel);
     int i, contadordepontos=0, teste, teste2;
     for(i=0;i<n;i++){
+        //printf("no - %d\n",array[i][0]);
         if(array[i][1] == 0){
             
             int proximo=i+1, fixo;
@@ -29,21 +32,24 @@ void calcularpontosmaximos(int valor ,int n){
                             proximo++;
                         }
                     }while(array[proximo][0] == array[i][0]);
-                    /*for(teste2=0;teste2<2;teste2++){
+                    
+                    for(teste2=0;teste2<2;teste2++){
                         for(teste=0;teste<n;teste++){
                             printf("%d ",array[teste][teste2]);
                         }
                         printf("\n");
                     }
-                    printf("\n");*/
-                    calcularpontosmaximos(valor+(contadordepontos*(contadordepontos+1)), n);
+                    printf("\n");
+                    
+                    calcularpontosmaximos(valor+(contadordepontos*(contadordepontos+1)),nivel+1 ,n);
+                    
                     contadordepontos=0;
                     proximo=fixo;
                     array[i][1]=0;
                     do{
                         array[proximo][1]=0;
                         proximo++;
-                        while(array[proximo][1] == 1 && array[proximo][0] != array[i][0]){
+                        while(array[proximo][1] == 1 && array[proximo][0] != array[i][0] && proximo<n){
                             proximo++;
                         }
                     }while(array[proximo][0] == array[i][0]);
@@ -54,12 +60,24 @@ void calcularpontosmaximos(int valor ,int n){
     if(pontosmaximo<valor){
         pontosmaximo = valor;
     }
+        int lolololol=0, aux=0;
+        for(lolololol=0;lolololol<n;lolololol++){
+            if(array[lolololol][1] == 0){
+                aux = 1;
+            }
+        }
+        if(aux == 0){
+            continua = 1;
+        }
+           
+}
 }
 
 int main() {
     int caso,numero_testes,n, i;
     scanf("%d", &numero_testes);
     for(caso=0;caso<numero_testes;caso++){
+        
         pontosmaximo=0;
         scanf("%d", &n);
         
@@ -68,9 +86,7 @@ int main() {
             array[i][1]=0;
         }
         
-        
-        
-        calcularpontosmaximos(0,n);
+        calcularpontosmaximos(0,0,n);
         printf("%d\n", pontosmaximo);
     }
     
