1c433e56b1e5a9749135ad63d8b5ac8ab6cd992f
Tiago Andrade <tagowill@gmail.com>
2015-02-26 15:54:24 +0000

    So com TLE week3

--- weektree/weektree.c
+++ weektree/weektree.c
@@ -7,9 +7,14 @@ int pontosmaximo=0;
 static int array[N][2];
 int continua=0;
 
-void calcularpontosmaximos(int valor, int nivel ,int n){
+void calcularpontosmaximos(int valor,int n){
     
-    int i, contadordepontos=0;
+    int i, contadordepontos=0, j;
+    int pintei[N];
+    if(pontosmaximo<valor){
+        pontosmaximo = valor;
+    }
+    
     for(i=0;i<n;i++){
 
         if(array[i][1] == 0){
@@ -23,28 +28,29 @@ void calcularpontosmaximos(int valor, int nivel ,int n){
                 if(array[i][0] == array[proximo][0]){
                     contadordepontos++;
                     array[i][1]=1;
+                    pintei[i] = 1;
                     do{
                         contadordepontos++;
                         array[proximo][1]=1;
+                        pintei[proximo]=1;
                         proximo++;
                         while(array[proximo][1] == 1 && proximo<n){
                             proximo++;
                         }
                     }while(array[proximo][0] == array[i][0] && proximo<n);
+
                     
-                    calcularpontosmaximos(valor+(contadordepontos*(contadordepontos+1)),nivel+1 ,n);
+                    calcularpontosmaximos(valor+(contadordepontos*(contadordepontos+1)),n);
                     
                     contadordepontos=0;
-
-                    array[i][1]=0;
-                    do{
-                        array[proximo][1]=0;
-                        proximo--;
-                        while(array[proximo][0] != array[i][0] && proximo>fixo){
-                            proximo--;
+                    
+                    for(j=0;j<n;j++){
+                        if(pintei[j]==1){
+                            pintei[j]=0;
+                            array[j][1]=0;
                         }
-                    }while(array[proximo][0] == array[i][0] && proximo>fixo);
-
+                    }
+                    
                     proximo = i+1;
                     while(array[i][0]==array[proximo][0] && proximo<n){
                         i++;
@@ -55,9 +61,7 @@ void calcularpontosmaximos(int valor, int nivel ,int n){
             }
         }
     }
-    if(pontosmaximo<valor){
-        pontosmaximo = valor;
-    }
+
 }
 
 int main() {
@@ -73,7 +77,7 @@ int main() {
             array[i][1]=0;
         }
         
-        calcularpontosmaximos(0,0,n);
+        calcularpontosmaximos(0,n);
         printf("%d\n", pontosmaximo);
     }
     
