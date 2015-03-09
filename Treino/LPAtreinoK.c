ac5be6ed2c5e45239234dd862d0134747948e810
Tiago Andrade <tagowill@gmail.com>
2015-03-09 18:17:24 +0000

    mais treinos incompletos

---
+++ Treino/LPAtreinoK.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int pizzas[61];
+int media=0, tamanho;
+
+int calcularTempos(int posi, int forno1, int forno2){
+    int primeira = 0, segunda = 0;
+    if(posi < tamanho){
+        if(forno1 < media){
+            primeira = calcularTempos(posi+1, forno1+pizzas[posi], forno2);
+        }else{
+            media = media * 2;
+            primeira = forno1;
+        }
+        segunda = calcularTempos(posi+1, forno1, forno2+pizzas[posi]);
+    }else{
+        if(forno1 < forno2){
+            return (forno2-forno1);
+        }else{
+            return forno1-forno2;
+        }
+    }
+    if(primeira<segunda){
+        return primeira;
+    }else{
+        return segunda;
+    }
+}
+
+int main() {
+    
+    int i;
+    int minimo;
+    float aux;
+    
+    while(scanf("%d", &tamanho) != EOF){
+        for(i=0;i<tamanho;i++){
+            scanf("%f", &aux);
+            pizzas[i] = aux * 100;
+            media += pizzas[i];
+        }
+        
+        media = media / 2;
+        
+        minimo = calcularTempos(1, pizzas[0], 0);
+        
+        aux = minimo / 100.0;
+        
+        printf("%.2f\n", aux);
+    }
+    
+    return 0;
+}
---
+++ Treino/LPAtreinoO.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void verificaCoisas(int posi, int ){
+    
+}
+
+int main() {
+    int n, m, i;
+    
+    scanf("%d %d", &n, &m);
+    int ligacoes[n][2];
+    int usado[n];
+    
+    for(i=0;i<m;i++){
+        scanf("%d %d", &ligacoes[i][0], &ligacoes[i][1]);
+    }
+
+    return 0;
+}
