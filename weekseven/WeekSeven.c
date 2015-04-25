1727d163d1bea05a61577d25c2aa960e9d344786
Tiago Andrade <tagowill@gmail.com>
2015-04-25 23:55:32 +0000

    Primeira tentativa

---
+++ weekseven/WeekSeven.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 1000
+
+struct dia{
+    int populacao;
+    int depedencias;
+    int ligacoes;
+    int ponteiros[N];
+    int usado;
+};
+
+struct dia tabela[N];
+int total;
+int semanas=0;
+
+int main(){
+    
+    int i, j;
+    int populacao=0, nosligados=0;
+    int n;
+    int contador=0, contador2=0;
+    int aux[N], aux2[N];
+    while(scanf("%d %d", &n, &total)!=EOF){
+    
+    /*Inicializa*/
+    for(i=0;i<n;i++){
+        tabela[i].depedencias = 0;
+        tabela[i].ligacoes = 0;
+        tabela[i].usado=0;
+    }
+    
+    /*Contruir array de estrutura*/
+    for(i=0;i<n;i++){
+        int id;
+        scanf("%d %d", &populacao, &nosligados);
+        tabela[i].populacao = populacao;
+        tabela[i].ligacoes = nosligados;
+        for(j=0;j<nosligados;j++){
+            scanf("%d", &id);
+            tabela[i].ponteiros[j] = id;
+            tabela[id].depedencias++;
+        }
+    }
+    
+    /*buscar os primeiros elementos*/
+    total = total - tabela[0].populacao;
+    tabela[i].usado =1;
+    aux[contador] = 0;
+    contador++;
+    for(i=1; i<n; i++){
+        if(tabela[i].depedencias == 0){
+            tabela[i].usado=1;
+            aux[contador] = i;
+            contador++;
+        }
+    }
+    semanas++;
+    /*Verificar se existem nos para continuar*/
+    if(total<=0){
+        printf ("%d\n", semanas);
+    }
+    
+    int *a, *b, *c;
+    
+    a = aux;
+    b = aux2;
+    j=0;
+    int posso = 0;
+    while(contador != 0 && posso==0){
+        contador2=0;
+        while(j<contador && posso == 0){
+            for(i=0;i<tabela[a[j]].ligacoes;i++){
+                if(tabela[tabela[a[j]].ponteiros[i]].usado == 0){
+                    tabela[tabela[a[j]].ponteiros[i]].usado = 1;
+                    total = total - tabela[tabela[a[j]].ponteiros[i]].populacao;
+                    b[contador2] = tabela[a[j]].ponteiros[i];
+                    contador2++;
+                }
+            }
+            j++;
+            
+        };
+        semanas++;
+        contador = contador2;
+        j=0;
+        c = a;
+        a = b;
+        b = c;
+        if(total<=0){
+            //printf("%d\n", semanas);
+            posso=1;
+        }
+    }
+    if(total<=0){
+        printf("%d\n", semanas);
+    }else{
+        printf("Safe!\n");
+    }
+    posso=0;
+    }
+    return 0;
+}
